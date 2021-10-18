3e8401e03cb7626f3c33dedc9362057f3976f573
Daniel25778 <danielwilsonca94@gmail.com>
2021-10-18 11:23:42 -0300

    atualizacao-redirecionamento-login para -nao tem login - KOTLIN

--- app/src/main/java/com/example/relativelayout/LoginActivity.kt
+++ app/src/main/java/com/example/relativelayout/LoginActivity.kt
@@ -1,12 +1,23 @@
 package com.example.relativelayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         supportActionBar!!.hide()
+
+        val naoTemConta = findViewById<TextView>(R.id.text_nao_tem_conta)
+
+        naoTemConta.setOnClickListener {
+            val abrirNovoUsuario = Intent( this, NovoUsuarioActivity::class.java )
+            startActivity(abrirNovoUsuario)
+
+        }
+
     }
 }
--- app/src/main/java/com/example/relativelayout/MainActivity.kt
+++ app/src/main/java/com/example/relativelayout/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.relativelayout
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 
 
@@ -12,14 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val naoTemConta = findViewById<TextView>(R.id.text_nao_tem_conta)
-
-        naoTemConta.setOnClickListener {
-            val abrirNovoUsuario = Intent( this, NovoUsuarioActivity::class.java )
-            startActivity(abrirNovoUsuario)
-
-           }
 
 
 
